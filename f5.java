d6a4f6281138f5c69feaec9973829f65e7e7c778
tousif1504 <54286896+tousif1504@users.noreply.github.com>
2019-08-21 01:31:47 +0530

    Update f5.java

--- f5.java
+++ f5.java
@@ -1,7 +1,7 @@
 class f5
-{
- public static void main(string arg[])
  {
-  system.out.println("this is simple java code");
-  }
+  public static void main(string arg[])
+   {
+    system.out.println("this is simple java code");
+    }
 }
